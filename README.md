38e45886a6a1743b957a127554f6b82af20c810a
Camille Wiedemann <camille@MacBook-Pro-de-Camille.local>
2018-01-05 22:44:59 +0400

    ajout du diagramme radar des etats

--- README.md
+++ README.md
@@ -49,9 +49,11 @@ _check when the data has been imported on the repository_
 
 __Here are the diferent methods we aim to implement __ : 
 - [x] statistique descriptives: moy et écart type des principales variables
+- [x] diagramme radar (en étoile) pour comparer visuellement les états
 - [x] analyse en composantes principales (R)
 - [x] méthodes de classification automatique pour représenter les données et émettre des hypothèses de dépendance entre certaines variables --> cluster, k-means (R) 
-- [ ] états plus ou moins écolo : corréler les cluster avec des variables quantitatives (rep, dem, ...) --> test du chi 2 pour voir si même cluster
+- [x] états plus ou moins écolo : corréler les cluster avec des variables quantitatives (rep, dem, ...)
+- [ ] test du chi 2 pour voir si même cluster (definis et obtenus)
 - [ ] test d'adéquation des données à une loi ou à une famille de loi (selon la nature des données, test du chi-2, tests de normalité, test de Kolmogorov) (juste pour une partie not. facteurs sociaux)
 - [ ] estimation des paramètres par intervalles de confiance --> surtout pour les résultat de la régression linéaire --> 50 var assez
 - [ ] analyse de variance à un facteur pour des échantillons multiples --> hypothese gaussienne ou test d'homogénéité non paramétrique mais avec seulement 2 types de données (sur le type de flotte)
---
+++ radar.R
@@ -0,0 +1,51 @@
+#On efface les variables en mémoire, permet de faire le ménage avant de commencer
+rm(list=ls())
+
+statenames=c("ALABAMA", "ALASKA", "ARIZONA", "ARKANSAS", "CALIFORNIA", "COLORADO", "CONNECTICUT", "DELAWARE", "DISTRICT OF COLUMBIA", "FLORIDA", "GEORGIA", "HAWAII", "IDAHO", "ILLINOIS", "INDIANA", "IOWA", "KANSAS", "KENTUCKY", "LOUISIANA", "MAINE", "MARYLAND", "MASSACHUSETTS", "MICHIGAN", "MINNESOTA", "MISSISSIPPI", "MISSOURI", "MONTANA", "NEBRASKA", "NEVADA", "NEW HAMPSHIRE", "NEW JERSEY", "NEW MEXICO", "NEW YORK", "NORTH CAROLINA", "NORTH DAKOTA", "OHIO", "OKLAHOMA", "OREGON", "PENNSYLVANIA", "RHODE ISLAND", "SOUTH CAROLINA", "SOUTH DAKOTA", "TENNESSEE", "TEXAS", "UTAH", "VERMONT", "VIRGINIA", "WASHINGTON", "WEST VIRGINIA", "WISCONSIN", "WYOMING")
+
+###################################################################
+########################################### chargement des données
+
+data_tab=read.table("DataEasyNameOnlyStates.csv",header=TRUE,sep=";", dec=",")
+class(data_tab) 
+data_toAnalyse=data_tab[1:51,c(4,5,6,8,9,11,13,14,15,23)]
+maxdata=data_toAnalyse[1,]
+mindata=data_toAnalyse[1,]
+for (k in 1:10){
+  maxdata[k]=max(data_toAnalyse[,k])
+  mindata[k]=min(data_toAnalyse[,k])
+}
+data_toAnalyse = rbind(maxdata, mindata, data_toAnalyse)
+rownames(data_toAnalyse)= c("Max", "Min", statenames)
+  
+###################################################################
+############ diagramme de Kiviat (radar chart) pour tous les états
+
+#install.packages("fmsb")
+library(fmsb)
+
+par(mfrow = c(3, 3), mar = c(1,1,1,1)) 
+for (name in statenames[1:9]){ 
+  radarchart(data_toAnalyse[c("Max", "Min", name),], axistype = 2, seg = 10, plty=1, maxmin = TRUE, title = name) 
+}
+
+COL=colorRampPalette(c("firebrick","cadetblue","chocolate","burlywood","chartreuse","darkmagenta","aquamarine","forestgreen","blueviolet","darkgoldenrod","cornflowerblue","coral","darkseagreen","deeppink","gold","darkred"))(51) 
+par(mfrow = c(3, 3), mar = c(1,1,1,1))
+for (i in 3:11){ 
+  radarchart(data_toAnalyse[c(1,2,i),], axistype = 2, seg = 10, pcol = COL[i-2], cglcol = "grey80", plty=1, maxmin = TRUE, title = rownames(data_toAnalyse[i,])) 
+}
+for (i in 12:20){ 
+  radarchart(data_toAnalyse[c(1,2,i),], axistype = 2, seg = 10, pcol = COL[i-2], cglcol = "grey80", plty=1, maxmin = TRUE, title = rownames(data_toAnalyse[i,])) 
+}
+for (i in 21:29){ 
+  radarchart(data_toAnalyse[c(1,2,i),], axistype = 2, seg = 10, pcol = COL[i-2], cglcol = "grey80", plty=1, maxmin = TRUE, title = rownames(data_toAnalyse[i,])) 
+}
+for (i in 30:38){ 
+  radarchart(data_toAnalyse[c(1,2,i),], axistype = 2, seg = 10, pcol = COL[i-2], cglcol = "grey80", plty=1, maxmin = TRUE, title = rownames(data_toAnalyse[i,])) 
+}
+for (i in 39:47){ 
+  radarchart(data_toAnalyse[c(1,2,i),], axistype = 2, seg = 10, pcol = COL[i-2], cglcol = "grey80", plty=1, maxmin = TRUE, title = rownames(data_toAnalyse[i,])) 
+}
+for (i in 48:52){ 
+  radarchart(data_toAnalyse[c(1,2,i),], axistype = 2, seg = 10, pcol = COL[i-2], cglcol = "grey80", plty=1, maxmin = TRUE, title = rownames(data_toAnalyse[i,])) 
+}
